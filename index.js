85f65ab356e2934ae2b3d4d651a95eab937a2901
papa2008 <112895777+papa2008@users.noreply.github.com>
2024-08-31 18:03:09 +0200

    g

---
+++ index.js
@@ -0,0 +1,31 @@
+//1.
+
+
+ let result = 5+5+'5'
+ console.log(result);
+ console.log(typeof(result));
+
+//2.
+
+let myEmail = "alexandrnick@gmail.com";
+console.log(myEmail.includes("@"));
+console.log(myEmail.length);
+
+//3. 
+
+ const my = "My";;
+ const myName = "name";
+ const is = "is";
+ const fullName = `Hello ${my} ${myName} ${is} Sasha.`
+ console.log(fullName);
+
+
+//4. 
+ let userName = "Alex";
+ let paymentCheck = "20$";
+ let alertPaymentCheck = `Thank's, Alex for paying 20$ `;
+ alert(alertPaymentCheck);
+
+
+
+
