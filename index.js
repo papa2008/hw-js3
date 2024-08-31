1b0a73c91ccfeec7f0be515e024c6da3c0d1679a
papa2008 <112895777+papa2008@users.noreply.github.com>
2024-08-31 18:28:24 +0200

    и

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
-//1.
+//1.  
+let result = 5+5+'5'
+console.log(result);
+console.log(typeof(result));
 
-
- let result = 5+5+'5'
- console.log(result);
- console.log(typeof(result));
-
-//2.
-
+//2. 
 let myEmail = "alexandrnick@gmail.com";
 console.log(myEmail.includes("@"));
 console.log(myEmail.length);
 
 //3. 
-
- const my = "My";;
- const myName = "name";
- const is = "is";
- const fullName = `Hello ${my} ${myName} ${is} Sasha.`
- console.log(fullName);
+const my = "My";;
+const myName = "name";
+const is = "is";
+const fullName = `Hello ${my} ${myName} ${is} Sasha.`
+console.log(fullName);
 
 
 //4. 
- let userName = "Alex";
- let paymentCheck = "20$";
- let alertPaymentCheck = `Thank's, Alex for paying 20$ `;
- alert(alertPaymentCheck);
+let userName = "Alex";
+let paymentCheck = "20$";
+let alertPaymentCheck = `Thank's, ${userName} for paying ${paymentCheck}`;
+alert(alertPaymentCheck);
 
 
 
